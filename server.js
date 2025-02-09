fd6b4dde0df400101e62ef04dea8ed7d98f29e67
Ankit Kumar <abhishekankit128@gmail.com>
2025-02-09 23:32:11 +0530

    m

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoute');
 const cors = require('cors');
 
-connectDB();
 const app = express();
 
 app.use(cors());
@@ -12,4 +11,5 @@ app.use(express.json());
 
 app.use('/api/users', userRoutes);
 
+connectDB();
 app.listen(5000, () => console.log('Server running on port 5000'));
